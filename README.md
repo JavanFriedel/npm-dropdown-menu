01902951ac562154dcdf2d64793596cb4b5678bc
JavanFriedel <javanfriedel@hotmail.com>
2022-03-22 18:21:18 -0400

    Inital Commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# npm-dropdown-menu
+# What is this?
+
 A simple javascript dropdown menu.
+
+# Installation
+
+`npm install @javanfriedel/dropdown-menu --save`
+
+Then...
+
+```
+import {generateMenu} from '@javanfriedel/dropdown-menu'
+
+const container = <Your HTML Node you want replaced>;
+
+generateMenu(container);
+
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+function generateMenu(container = '') {
+  const children = container.children;
+
+  container.style.display = 'inline-block';
+
+  // LIST CREATION
+  const list = document.createElement('ul');
+  list.style.listStyle = 'none';
+  list.style.padding = '0';
+
+  for (let i = 0; i < children.length; i++) {
+    let li = document.createElement('li');
+    let a = children[i].querySelector('a');
+    if (a !== null) {
+      a.classList.add('burger-link');
+      li.append(a);
+    } else {
+      li.innerText = children[i].innerText;
+    }
+    li.classList.add('burger-li');
+    li.style.padding = '15px 25px';
+    list.appendChild(li);
+  }
+
+  const button = document.createElement('div');
+  button.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z"/></svg>';
+
+  button.classList.add('burger-3bar');
+
+  const dropDownBox = document.createElement('div');
+  dropDownBox.style.display = 'none';
+  dropDownBox.style.position = 'absolute';
+  dropDownBox.classList.add('burger-drop-box');
+
+  dropDownBox.appendChild(list);
+
+  button.addEventListener('mouseover', () => {
+    dropDownBox.style.display = 'block';
+  });
+
+  button.addEventListener('mouseleave', () => {
+    dropDownBox.style.display = 'none';
+  });
+
+  container.innerHTML = '';
+  button.append(dropDownBox);
+  container.append(button);
+}
+
+module.exports.generateMenu = generateMenu;
